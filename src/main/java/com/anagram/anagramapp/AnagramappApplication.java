2f33ca44eafba2d5f74b0c35ed4eeb3e66dc5a9e
meetdpv <43143734+meetdpv@users.noreply.github.com>
2020-08-27 00:24:01 -0400

    Adding initial code

---
+++ src/main/java/com/anagram/anagramapp/AnagramappApplication.java
@@ -0,0 +1,13 @@
+package com.anagram.anagramapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramappApplication.class, args);
+	}
+
+}
